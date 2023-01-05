26471c260ef693d329ab5002d7978beab58983c1
AlexDev404 <alexdev404@users.noreply.github.com>
2023-01-04 21:33:32 -0600

    Port Friend API

--- src/api/Friend/create.ts
+++ src/api/Friend/create.ts
@@ -8,15 +8,15 @@ const app = Router();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post(`${API_BASE}friend/addFriend`, async (req, res) => {
-  const { username, tagId } = req.body; // test
+app.post(`${API_BASE}friend/add`, async (req, res) => {
+  const { ID, username } = req.body; // test
 
-  const user = await User.findOne({ username, tagId });
+  const user = await User.findOne({ username, ID });
 
   const friendRequestData = await User.find({
     fromUser: username,
     toUser: user?._id,
-    tagId,
+    tagId: ID,
     status: "ADD",
   });
 
